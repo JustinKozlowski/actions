73e83c49fa20f53f3c4d929ece603f4c7ca5e97b
JustinKozlowski <jjkozlow@buffalo.edu>
2022-11-02 16:49:08 -0400

    secrest test

---
+++ autolab.py
@@ -0,0 +1,27 @@
+from time import sleep
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+
+print("hello test")
+
+# def main():
+#     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#     driver.get('https://autograder.cse.buffalo.edu/auth/users/sign_in')
+#     print("Current Page: " + driver.title)
+#     elem = driver.find_element(By.TAG_NAME, 'input')  # Find the search box
+#     elem.click()
+#     print("Current Page: " + driver.title)
+#     elem = driver.find_element(By.NAME, 'j_username')
+#     elem.send_keys(os.environ['UBIT_USERNAME'])
+#     elem = driver.find_element(By.NAME, 'j_password')
+#     elem.send_keys(os.environ['UBIT_PASSWORD'] + Keys.RETURN)
+#     print("Current Page: " + driver.title)
+#     sleep(1)
+#     print("Current Page: " + driver.title)
+#     driver.quit()
+
+# if __name__ == "__main__":
+#     main()
