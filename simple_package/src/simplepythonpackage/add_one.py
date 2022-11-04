1d195e8b481e3fa18cb4f5ba58280b3937f1f121
JustinKozlowski <jjkozlow@buffalo.edu>
2022-11-04 15:13:12 -0400

    new tests

--- simple_package/src/simplepythonpackage/add_one.py
+++
@@ -1,7 +0,0 @@
-"""A function that adds 1"""
-
-
-def add_one(num):
-    """A function that adds 1"""
-    return num + 1
-    # x = 1
---
+++ simple_package/tests/test_commit_message.py
@@ -0,0 +1,13 @@
+from commitparser.parse_commit import parse_commit
+
+
+def test_commit_message():
+    assert parse_commit('submit ~UB Hacking Submission~ parse_commit.py') == ('UB Hacking Submission', ['parse_commit.py'])
+
+
+def test_add_one_z():
+    try:
+        parse_commit(1)
+        assert False
+    except AttributeError:
+        assert True
