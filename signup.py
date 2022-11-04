c6e826b34861757d92c8ed09b100a97f995b8086
JustinKozlowski <jjkozlow@buffalo.edu>
2022-11-04 17:12:38 -0400

    auto register

--- signup.py
+++ signup.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-from commitparser.parse_commit import parse_commit
 
 
 def main():
